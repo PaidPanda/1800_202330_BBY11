535e44e015db3820df6be8e125d512ee70da3f41
Nathan Mak <nmak18@my.bcit.ca>
2023-11-14 01:50:42 -0800

    updated hard code for lot details in lotdetail.html and created js functions to read and populate information from Firestore lots collection to lotdetails.html

--- scripts/parkinglots.js
+++ scripts/parkinglots.js
@@ -1,5 +1,4 @@
 
-
 function writeLots() {
     //define a variable for the collection you want to create in Firestore to populate data
     var lotsRef = db.collection("lots");
@@ -10,7 +9,7 @@ function writeLots() {
         city: "Burnaby",
         province: "BC",
         status: "Full",
-		details: "A lovely place for lunch walk",
+		details: "Accessible Parking, Motorcylce Area",
         last_updated: firebase.firestore.FieldValue.serverTimestamp()   //current system time
     });
     lotsRef.add({
@@ -19,26 +18,17 @@ function writeLots() {
         city: "Burnaby",
         province: "BC",
         status: "Busy",
-        details: "Close to town, and relaxing",
+        details: "N/A",
         last_updated: firebase.firestore.Timestamp.fromDate(new Date("March 10, 2022"))
-    });
-    lotsRef.add({
-        code: "BBYC",
-        name: "Parking Lot C", 
-        city: "Burnaby",
-        province: "BC",
-        status: "Vacant",
-        details:  "Amazing ski slope views",
-        last_updated: firebase.firestore.Timestamp.fromDate(new Date("January 1, 2023"))
     });
     lotsRef.add({
         code: "BBYD",
         name: "Parking Lot D", 
         city: "Burnaby",
         province: "BC",
-        status: "Full",
-		details: "A lovely place for lunch walk",
-        last_updated: firebase.firestore.FieldValue.serverTimestamp()
+        status: "Vacant",
+        details:  "Accessible Parking, Pay Station",
+        last_updated: firebase.firestore.Timestamp.fromDate(new Date("January 1, 2023"))
     });
     lotsRef.add({
         code: "BBYE",
@@ -46,7 +36,7 @@ function writeLots() {
         city: "Burnaby",
         province: "BC",
         status: "Full",
-		details: "A lovely place for lunch walk",
+		details: "Carshare Parking",
         last_updated: firebase.firestore.FieldValue.serverTimestamp()
     });
     lotsRef.add({
@@ -54,17 +44,93 @@ function writeLots() {
         name: "Parking Lot F", 
         city: "Burnaby",
         province: "BC",
-        status: "Busy",
-		details: "A lovely place for lunch walk",
+        status: "Full",
+		details: "Accessible Parking",
         last_updated: firebase.firestore.FieldValue.serverTimestamp()
     });
     lotsRef.add({
-        code: "BBYG",
-        name: "Parking Lot G", 
+        code: "BBYN",
+        name: "Parking Lot N", 
+        city: "Burnaby",
+        province: "BC",
+        status: "Busy",
+		details: "Pay Station",
+        last_updated: firebase.firestore.FieldValue.serverTimestamp()
+    });
+    lotsRef.add({
+        code: "BBYQ",
+        name: "Parking Lot Q", 
         city: "Burnaby",
         province: "BC",
         status: "Vacant",
-		details: "A lovely place for lunch walk",
+		details: "Carshare Parking",
         last_updated: firebase.firestore.FieldValue.serverTimestamp()
     });
 }
+
+// Function to read the name of the parking lot from Firestore "lots" collection
+// Input param is the String representing the parking lot, aka, the document name
+function readName(parkinglot) {
+    db.collection("lots").doc(parkinglot)                                                      //name of the collection and documents should matach excatly with what you have in Firestore
+      .onSnapshot(parkinglotDoc => {                                                               //arrow notation
+           console.log("current document data: " + parkinglotDoc.data());                          //.data() returns data object
+           document.getElementById("name-goes-here").innerHTML = parkinglotDoc.data().name;      //using javascript to display the data on the right place
+           
+           //Here are other ways to access key-value data fields
+           //$('#quote-goes-here').text(dayDoc.data().quote);         //using jquery object dot notation
+           //$("#quote-goes-here").text(dayDoc.data()["quote"]);      //using json object indexing
+		       //document.querySelector("#quote-goes-here").innerHTML = dayDoc.data().quote;
+      })
+}
+readName("LoNPtlttHuemMG8tcrEa");        //calling the function
+
+
+// Function to read the lots details from Firestore "lots" collection
+// Input param is the String representing the parking lot, aka, the document name
+function readDetails(parkinglot) {
+    db.collection("lots").doc(parkinglot)                                                      //name of the collection and documents should matach excatly with what you have in Firestore
+      .onSnapshot(parkinglotDoc => {                                                               //arrow notation
+           console.log("current document data: " + parkinglotDoc.data());                          //.data() returns data object
+           document.getElementById("details-goes-here").innerHTML = parkinglotDoc.data().details;      //using javascript to display the data on the right place
+           
+           //Here are other ways to access key-value data fields
+           //$('#quote-goes-here').text(dayDoc.data().quote);         //using jquery object dot notation
+           //$("#quote-goes-here").text(dayDoc.data()["quote"]);      //using json object indexing
+		       //document.querySelector("#quote-goes-here").innerHTML = dayDoc.data().quote;
+      })
+}
+readDetails("LoNPtlttHuemMG8tcrEa");        //calling the function
+
+
+// Function to read the lot availability status from Firestore "lots" collection
+// Input param is the String representing the parking lot, aka, the document name
+function readStatus(parkinglot) {
+    db.collection("lots").doc(parkinglot)                                                      //name of the collection and documents should matach excatly with what you have in Firestore
+      .onSnapshot(parkinglotDoc => {                                                               //arrow notation
+           console.log("current document data: " + parkinglotDoc.data());                          //.data() returns data object
+           document.getElementById("status-goes-here").innerHTML = parkinglotDoc.data().status;      //using javascript to display the data on the right place
+           
+           //Here are other ways to access key-value data fields
+           //$('#quote-goes-here').text(dayDoc.data().quote);         //using jquery object dot notation
+           //$("#quote-goes-here").text(dayDoc.data()["quote"]);      //using json object indexing
+		       //document.querySelector("#quote-goes-here").innerHTML = dayDoc.data().quote;
+      })
+}
+readStatus("LoNPtlttHuemMG8tcrEa");        //calling the function
+
+
+// Function to read the when the status was last updated from Firestore "lots" collection
+// Input param is the String representing the parking lot, aka, the document name
+function readLastUpdate(parkinglot) {
+    db.collection("lots").doc(parkinglot)                                                      //name of the collection and documents should matach excatly with what you have in Firestore
+      .onSnapshot(parkinglotDoc => {                                                               //arrow notation
+           console.log("current document data: " + parkinglotDoc.data());                          //.data() returns data object
+           document.getElementById("lastupdate-goes-here").innerHTML = parkinglotDoc.data().last_updated;      //using javascript to display the data on the right place
+           
+           //Here are other ways to access key-value data fields
+           //$('#quote-goes-here').text(dayDoc.data().quote);         //using jquery object dot notation
+           //$("#quote-goes-here").text(dayDoc.data()["quote"]);      //using json object indexing
+		       //document.querySelector("#quote-goes-here").innerHTML = dayDoc.data().quote;
+      })
+}
+readLastUpdate("LoNPtlttHuemMG8tcrEa");        //calling the function
